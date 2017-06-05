892dc6c2d4c9ec7e71a764781a37dac02109bfc5
Eric Mouledoux <EricMouledoux@gmail.com>
2017-06-05 15:56:08 -0500

    Init Commit
    Whole project

---
+++ Assets/Scripts/Fly.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly : MonoBehaviour
+{
+    [SerializeField]
+    private float m_maxHeight;
+
+    [SerializeField]
+    private AudioSource m_audioSource;
+
+    [SerializeField]
+    private UnityEngine.UI.Slider m_slider;
+
+    private Vector3 m_swayPos = Vector3.zero;
+
+    // Use this for initialization
+    void Start()
+    {
+        UnityEngine.UI.Slider.SliderEvent onSlide = new UnityEngine.UI.Slider.SliderEvent();
+        onSlide.AddListener(delegate { AdjustAllValuesToSlider(); });
+        m_slider.onValueChanged = onSlide;
+
+        AdjustAllValuesToSlider();
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void AdjustAllValuesToSlider()
+    {
+        float height = (m_maxHeight * ((m_slider.value - m_slider.minValue) / m_slider.maxValue));
+        transform.localPosition = new Vector3(0, height, 0);
+        GetComponent<Animator>().speed = m_slider.value * 5;
+        m_audioSource.pitch = 1 + (height / m_maxHeight);
+    }
+
+    public void ResetAllValues()
+    {
+        transform.localPosition = Vector3.zero;
+        GetComponent<Animator>().speed = 1;
+        m_audioSource.pitch = 1;
+    }
+
+    private void SetAnimationToSlider()
+    {
+        GetComponent<Animator>().speed = m_slider.value;
+    }    
+}
---
+++ Assets/Scripts/OnRender.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnRender : MonoBehaviour
+{
+    Renderer m_renderer;
+    bool m_wasRendering = false;
+
+    public UnityEngine.Events.UnityEvent OnVisible;
+    public UnityEngine.Events.UnityEvent OnHide;
+
+    private void Start()
+    {
+        m_renderer = GetComponent<Renderer>();
+        StartCoroutine(_DelayedUpdate(1f));
+    }
+
+    public IEnumerator _DelayedUpdate(float delayTime)
+    {
+        while (Application.isPlaying)
+        {
+
+            if (m_renderer.isVisible == true && m_wasRendering == false)
+            {
+                OnVisible.Invoke();
+            }
+
+
+            else if (m_renderer.isVisible == false && m_wasRendering == true)
+            {
+                OnHide.Invoke();
+            }
+
+            m_wasRendering = m_renderer.isVisible;
+
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+}
